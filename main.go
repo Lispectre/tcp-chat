ada8ff7471a0b3ab54f933016753d2c4749f33e9
Lispectre <55454477+Lispectre@users.noreply.github.com>
2024-04-07 14:24:34 +0200

    basics

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-  fmt.Println("Hello, world")
+  if len(os.Args) < 2 {
+    fmt.Println("Not enough arguments specified. Exiting")
+    os.Exit(1)
+  }
+
+
+  switch mode := os.Args[1]; mode {
+    case "server":
+      fmt.Println("Starting a server...")
+      server_mode()
+    case "client":
+      fmt.Println("Starting as client...")
+      client_mode()
+    default: 
+      fmt.Println("Unknown mode specified! Exiting.")
+  }
+}
+
+func server_mode() {
+  if err := start_server(); err != nil {
+    log.Println(err)
+  }
+}
+func client_mode() {
+  if err := start_client(); err != nil {
+    log.Println(err)
+  }
 }
